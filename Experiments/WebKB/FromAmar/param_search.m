069748ae847ab1e1214f0fd37b36305f83bbbe21
matanorb <matanorb@4c94cbd0-a001-4ae1-9522-79914504ef8a>
2011-12-18 16:27:52 +0000

    Initial version
    git-svn-id: https://matano1-tp.haifa.ibm.com/svn/taco@13 4c94cbd0-a001-4ae1-9522-79914504ef8a

---
+++ Experiments/WebKB/FromAmar/param_search.m
@@ -0,0 +1,61 @@
+%% global shared parameters
+numRunsPerExperiment = 5;
+showResultsDuringRun = 0;
+graphFileName = 'C:\courses\theses\WebKB\data\From Amar\webkb_amar.mat';
+
+%% define parameter properties
+
+% K.range = [1,2,5,10,20,50,100,500];
+K.range = [20];
+K.name = 'K';
+alpha.range = [0.1,1,10];
+alpha.name = 'alpha';
+beta.range = [0.1,1,10];
+beta.name = 'beta';
+labeledConfidence.range = [0.1,1,10];
+labeledConfidence.name = 'labeledConfidence';
+
+paramProperties = [K, alpha, beta, labeledConfidence];
+
+%% create parameters structures
+
+params = createParamsVector(paramProperties);
+paramStructs = createParamStructs(paramProperties, params);
+
+%%
+
+tic;
+numStructs = length(paramStructs);
+allExperiments = [];
+for struct_i=1:numStructs
+    currentParams = paramStructs(struct_i);
+    
+    algorithmParams = currentParams.algorithmParams;
+    algorithmParams.numIterations = 50;
+    
+    constructionParams = currentParams.constructionParams;
+    constructionParams.numLabeled = 50;
+    constructionParams.numInstancesPerClass = 500;
+
+    experiment.result  = ...
+        avg_experiment( graphFileName, numRunsPerExperiment, ...
+                        constructionParams, algorithmParams, ...
+                        showResultsDuringRun);
+    experiment.params = currentParams;
+    experiment.fileName = graphFileName;
+    allExperiments = [ allExperiments; experiment ];
+end
+toc;
+
+%% Define which result figures to display
+figuresToShow.predictionAndConfidence = 0;
+figuresToShow.marginAndConfidence = 0;
+figuresToShow.assumulativeLoss = 1;
+
+%%
+numExperiments = length( allExperiments );
+for experiment_i=1:numExperiments
+    singleExperiment = allExperiments(experiment_i);
+    showResults( singleExperiment, figuresToShow );
+    %close all;
+end
