2dcfc4f0fa0788e128160948db281f7c8ea3a747
matanorb <matanorb@4c94cbd0-a001-4ae1-9522-79914504ef8a>
2011-12-18 10:08:03 +0000

    Moved
    git-svn-id: https://matano1-tp.haifa.ibm.com/svn/taco@7 4c94cbd0-a001-4ae1-9522-79914504ef8a

---
+++ Experiments/load_graph.m
@@ -0,0 +1,61 @@
+function [ outGraph, labeled ] =...
+    load_graph_amar(    graphFileName, classToLabelMap, ...
+                        K, numLabeledRequired, numInstancesPerClass )
+%LOADAMARGRAPH Summary of this function goes here
+%   Detailed explanation goes here
+
+%% index positions in the class to label map
+
+CLASS_VALUE = 1;
+LABEL_VALUE = 2;
+
+%% Load the graph
+
+% load 'C:\courses\theses\WebKB\data\From Amar\webkb_amar.mat';
+% graph = webkb_amar;
+graph = load( graphFileName, 'graph' );
+graph = graph.graph;
+
+%% select only required classes
+requiredClasses = classToLabelMap(:, CLASS_VALUE);
+[selected] = selectClasses(graph, requiredClasses);
+numRequiredClasses = length( requiredClasses );
+for class_i = 1:numRequiredClasses
+    classValue = classToLabelMap(class_i, CLASS_VALUE);
+    labelValue = classToLabelMap(class_i, LABEL_VALUE);
+    selected.labels(selected.labels == classValue) = labelValue;
+end
+
+%% Balance the classes
+if numInstancesPerClass ~= 0
+    [balanced ] = ...
+        balanceClasses(selected, numInstancesPerClass);
+else
+    balanced = selected;
+end;
+
+%isSymetric(balanced.weights);
+
+%% 
+
+w = balanced.weights;
+lbls = balanced.labels;
+
+%% Create K-Nearest Neighbour graph
+
+w_nn = knn(w,K);
+
+%% Randomly select labeled vertices
+
+labeled = zeros(numLabeledRequired, numRequiredClasses);
+for class_i = 1:numRequiredClasses
+    labelValue = classToLabelMap(class_i, LABEL_VALUE);
+    labeled(:, class_i) = ...
+        selectLabeled(lbls, numLabeledRequired, labelValue);
+end
+
+outGraph.weights = w_nn;
+outGraph.labels = lbls;
+
+end
+
