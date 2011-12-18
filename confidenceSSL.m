dd64296ca1b037197d00a40b9b7a6129d0259067
matanorb <matanorb@4c94cbd0-a001-4ae1-9522-79914504ef8a>
2011-12-18 09:49:28 +0000

    With regularized labels
    git-svn-id: https://matano1-tp.haifa.ibm.com/svn/taco@4 4c94cbd0-a001-4ae1-9522-79914504ef8a

--- confidenceSSL.m
+++ confidenceSSL.m
@@ -5,25 +5,23 @@ function iteration = confidenceSSL...
         labeledConfidence, alpha, beta)
 
     tic;
-            
+        
+    gamma = labeledConfidence;
     num_vertices = size(W,1);
     disp(['confidenceSSL. num vertices: ' num2str(num_vertices)]);
 	
 	iteration.mu = zeros( num_vertices, num_iterations );
-    iteration.v = ones( num_vertices, num_iterations );
+    iteration.v = ones( num_vertices, num_iterations ) * 10;
 
     labeled.positive = labeledPositive;
     labeled.negative = labeledNegative;
-    first_iteration = 1;
+    %first_iteration = 1;
 
-    iteration.mu( labeled.positive, first_iteration)  = positiveInitialValue;
-    iteration.mu( labeled.negative, first_iteration ) = negativeInitialValue;
+    %iteration.mu( labeled.positive, first_iteration)  = positiveInitialValue;
+    %iteration.mu( labeled.negative, first_iteration ) = negativeInitialValue;
 
-    iteration.v( labeled.positive, first_iteration)  = labeledConfidence;
-    iteration.v( labeled.negative, first_iteration ) = labeledConfidence;
-
-    %beta = this.beta;
-    %alpha = this.alpha;
+    %iteration.v( labeled.positive, first_iteration)  = labeledConfidence;
+    %iteration.v( labeled.negative, first_iteration ) = labeledConfidence;
 
     % note iteration index starts from 2
     for iter_i = 2:num_iterations
@@ -35,41 +33,70 @@ function iteration = confidenceSSL...
         prev_v =  iteration.v ( :, iter_i - 1) ;
 
         for vertex_i=1:num_vertices
+
+            isPositive = ismember(vertex_i, labeled.positive);
+            isNegative = ismember(vertex_i, labeled.negative);
+            isLabeled = isPositive | isNegative;
+            y_i =   isPositive * positiveInitialValue + ...
+                    isNegative * negativeInitialValue;
+            %isLabeled = 0.1 * isLabeled;
+            
             neighbours = find( W(vertex_i, :) ~= 0 );
             neighbours_w = W(vertex_i, neighbours);
             neighbours_w = neighbours_w.'; % make column vector
-            ni = sum( neighbours_w );
+            
+            ni = sum( neighbours_w ) + isLabeled;
             neighbours_mu = prev_mu( neighbours );
             neighbours_v  =  prev_v( neighbours );
-            B = sum( neighbours_w .* neighbours_mu );
-            C = sum( neighbours_w .* neighbours_mu ./ neighbours_v );
-            D = sum( neighbours_w ./ neighbours_v );
+            B = sum( neighbours_w .* neighbours_mu ) ...
+                + isLabeled * y_i;
+            C = sum( neighbours_w .* neighbours_mu ./ neighbours_v ) ...
+                + isLabeled * (y_i / gamma) ;
+            D = sum( neighbours_w ./ neighbours_v ) ...
+                + isLabeled / gamma;
+            
             %ni = length(neighbours);
             %B = sum( prev_mu( neighbours ) );
             %C = sum( prev_mu( neighbours ) ./ prev_v( neighbours ) );
             %D = sum( 1 ./ prev_v( neighbours ) );
-            iteration.mu(vertex_i, iter_i) = ...
-                (B + prev_v(vertex_i) * C) / (ni + prev_v(vertex_i) * D);
+            
+            new_mu =    (B + prev_v(vertex_i) * C) / ...
+                        (ni + prev_v(vertex_i) * D);
+            iteration.mu(vertex_i, iter_i) = new_mu ;
         end
 
-        iteration.mu( labeled.positive, iter_i)  = positiveInitialValue;
-        iteration.mu( labeled.negative,  iter_i ) = negativeInitialValue;
+        %prev_mu = iteration.mu( :, iter_i ) ;
+        %iteration.mu( labeled.positive, iter_i)  = positiveInitialValue;
+        %iteration.mu( labeled.negative,  iter_i ) = negativeInitialValue;
 
         for vertex_i=1:num_vertices
+            isPositive = ismember(vertex_i, labeled.positive);
+            isNegative = isMember(vertex_i, labeled.negative);
+            isLabeled = isPositive | isNegative;
+            y_i =   isPositive * positiveInitialValue + ...
+                    isNegative * negativeInitialValue;
+            %isLabeled = 0.1 * isLabeled;
+
             neighbours = find( W(vertex_i, :) ~= 0 );
             neighbours_w = W(vertex_i, neighbours);
             neighbours_w = neighbours_w.'; % make column vector
+            
+            mu_i = prev_mu(vertex_i);
+            neighbours_mu = prev_mu( neighbours );
             A = sum ( neighbours_w .* ...
-                (prev_mu(vertex_i) - prev_mu( neighbours )).^2 );
+                (mu_i  - neighbours_mu).^2 )...
+                + isLabeled * (mu_i -y_i)^2;
 
-            iteration.v(vertex_i, iter_i) = ...
-                (beta + sqrt( beta^2 + 4 * alpha * A)) / (2 * alpha);
+            new_v = (beta + sqrt( beta^2 + 2 * alpha * A))...
+                    / (2 * alpha);
+            iteration.v(vertex_i, iter_i) = new_v ;
+                
                 %(beta + sqrt( beta^2 + 4 * alpha * A)) / (2 * alpha);
                 % matan changed 5.12.11 from 4 to 2.
         end
 
-        iteration.v( labeled.positive, iter_i) = labeledConfidence;
-        iteration.v( labeled.negative, iter_i ) = labeledConfidence;
+        %iteration.v( labeled.positive, iter_i) = labeledConfidence;
+        %iteration.v( labeled.negative, iter_i ) = labeledConfidence;
 
     end
 
