54c9c205cc69e773097db07c08839d72d11a847f
matanorb <matanorb@4c94cbd0-a001-4ae1-9522-79914504ef8a>
2011-12-19 07:37:36 +0000

    Updated parameters to construction and algorithm parameters Saves complete experiment result changed name of accumulatedLoss field to sortedAccumulatedLoss
    git-svn-id: https://matano1-tp.haifa.ibm.com/svn/taco@14 4c94cbd0-a001-4ae1-9522-79914504ef8a

--- Experiments/WebKB/FromAmar/avg_experiment.m
+++ Experiments/WebKB/FromAmar/avg_experiment.m
@@ -1,29 +1,34 @@
 function total = avg_experiment...
-    ( numExperiments, K, numLabeled,...
-      numInstancesPerClass, num_iterations )
+    ( graphFileName, numExperiments, constructionParams, ...
+        algorithmParams, showResults )
+
+numInstancesPerClass = constructionParams.numInstancesPerClass;
 
 numVertices = numInstancesPerClass * 2;
-total.accumulativeLoss = zeros(numVertices, 1);
+total.sortedAccumulativeLoss = zeros(numVertices, 1);
 total.sortedConfidence = zeros(numVertices, 1);
 total.sortedMargin = zeros(numVertices, 1);
+total.completeResult = [];
+
 for i=1:numExperiments
-    exp = run_webkb_amar(K, numLabeled, ...
-        numInstancesPerClass, num_iterations);
+    exp = run_webkb_amar...
+        (graphFileName, constructionParams, ...
+        algorithmParams, showResults);
+    
     %r = exp.completeResult
     %for i=1:50 figure;scatter( 1:1000, r.mu(:,i) ); end
     
-    total.accumulativeLoss =...
-        total.accumulativeLoss + exp.accumulativeLoss;
+    total.sortedAccumulativeLoss =...
+        total.sortedAccumulativeLoss + exp.sortedAccumulativeLoss;
     total.sortedConfidence = ...
         total.sortedConfidence + exp.sortedConfidence;
     total.sortedMargin = ...
         total.sortedMargin + exp.sortedMargin;
-
-%     total.completeResults = ...
-%         [ total.completeResults 
+    total.completeResult = ...
+        [total.completeResult; exp.completeResult];
 end
 
-total.accumulativeLoss = total.accumulativeLoss / numExperiments;
+total.sortedAccumulativeLoss = total.sortedAccumulativeLoss / numExperiments;
 total.sortedConfidence = total.sortedConfidence / numExperiments;
 total.sortedMargin = total.sortedMargin / numExperiments;
 
