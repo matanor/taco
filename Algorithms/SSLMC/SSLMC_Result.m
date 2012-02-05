2cbd841513b175e57dc38e142cc2499ed1f4d222
matanorb <matanorb@4c94cbd0-a001-4ae1-9522-79914504ef8a>
2012-02-05 14:27:24 +0000

    SSLMC/SSLMC_Result.m: added getParams(), set_params() and some comments.
    git-svn-id: https://matano1-tp.haifa.ibm.com/svn/taco@73 4c94cbd0-a001-4ae1-9522-79914504ef8a

--- Algorithms/SSLMC/SSLMC_Result.m
+++ Algorithms/SSLMC/SSLMC_Result.m
@@ -1,104 +1,137 @@
 classdef SSLMC_Result < handle
 % Base class SSL multi-class (MC) algorithms result
 
-    properties (Access=public)
-        m_Y; % vertices X labels X iterations
+properties (Access=public)
+    m_Y; % vertices X labels X iterations
+    m_params;
+end
+
+properties (Access = protected)
+    BINARY_NUM_LABELS;
+    NEGATIVE; 
+    POSITIVE; 
+end % (Access = protected)
+
+methods (Access = public )
+
+    %% Constructor
+    
+    function this = SSLMC_Result() 
+        this.BINARY_NUM_LABELS = 2;
+        this.NEGATIVE = 1;
+        this.POSITIVE = 2;
     end
     
-	properties (Access = protected)
-        BINARY_NUM_LABELS;
-        NEGATIVE; 
-        POSITIVE; 
-    end % (Access = protected)
+    %% getParams
+    function R = getParams(this)
+        R = this.m_params;
+    end    
     
-    methods (Access = public )
-        
-        function this = SSLMC_Result() % Constructor
-            this.BINARY_NUM_LABELS = 2;
-            this.NEGATIVE = 1;
-            this.POSITIVE = 2;
+    %% set_params
+    
+    function set_params(this, value)
+        this.m_params = value;
+    end
+
+    %% prediction
+    
+    function r = prediction(this)
+        scoreMatrix = this.getFinalScoreMatrix();
+        [~,indices] = max(scoreMatrix,[],2);
+        r = indices;
+    end
+
+    %% predictionConfidence
+    
+    function r = predictionConfidence(this)
+        scoreMatrix = this.getFinalScoreMatrix();
+        [~,indices] = max(scoreMatrix,[],2);
+        confidence = zeros(this.numVertices(), 1);
+        for vertex_i=1:length(confidence)
+            predictedClass = indices(vertex_i);
+            confidence( vertex_i ) = this.getConfidence( vertex_i, predictedClass );
         end
-        
-        function r = prediction(this)
-            scoreMatrix = this.getFinalScoreMatrix();
-            [~,indices] = max(scoreMatrix,[],2);
-            r = indices;
-        end
-        
-        function r = predictionConfidence(this)
-            scoreMatrix = this.getFinalScoreMatrix();
-            [~,indices] = max(scoreMatrix,[],2);
-            confidence = zeros(this.numVertices(), 1);
-            for vertex_i=1:length(confidence)
-                predictedClass = indices(vertex_i);
-                confidence( vertex_i ) = this.getConfidence( vertex_i, predictedClass );
-            end
-            r = confidence;
-        end
-        
-        function r = this.getConfidence( this, ~, ~)
-            r = 1;
-        end
-        
+        r = confidence;
+    end
+
+    %% 
+    
+    function r = this.getConfidence( this, ~, ~)
+        r = 1;
+    end
+
 %         function r = scoreForLabel(this, label_i)
 %             scores = this.getFinalScoreMatrix();
 %             r = scores(:, label_i);
 %         end
-        
-        function r = getFinalScoreMatrix(this)
-%             disp('SSLMC::getFinalScoreMatrix');
-            r = this.m_Y(:,:,end);
-        end
 
-        function r = bestScore(this)
-            scoreMatrix = this.getFinalScoreMatrix();
-            [~,indices] = max(scoreMatrix,[],2);
-            bestScore = zeros(this.numVertices(), 1);
-            for vertex_i=1:length(bestScore)
-                bestScore( vertex_i ) = scoreMatrix( vertex_i, indices(vertex_i) );
-            end
-            r = bestScore;
-        end
-        
-        function r = binaryPrediction(this)
-            assert( this.numLabels() == (this.BINARY_NUM_LABELS) );
-            scoreMatrix = this.getFinalScoreMatrix();
-            [~,indices] = max(scoreMatrix,[],2);
-            scoreMatrix(:,this.NEGATIVE) = -scoreMatrix(:,this.NEGATIVE);
-            prediction = zeros(this.numVertices(), 1);
-            for vertex_i=1:length(prediction)
-                prediction( vertex_i ) = scoreMatrix( vertex_i, indices(vertex_i) );
-            end
-            r = prediction;
-        end
-        
-        function r = numVertices(this)
-            r = SSLMC_Result.calcVertices(this.m_Y);
-        end
-        
-        function r = numIterations(this)
-            r = SSLMC_Result.calcNumIterations(this.m_Y);
-        end
-        
-        function r = numLabels(this)
-            r = SSLMC_Result.calcNumLabels( this.getFinalScoreMatrix() );
-        end
-        
-    end % methods (Access = public )
+
+    %% getFinalScoreMatrix
     
-    methods (Static)
-        
-        function r = calcVertices( M )
-                r = size( M, 1 );
-        end
-        
-        function r = calcNumLabels( M )
-            r = size( M, 2 );
-        end
-        
-        function r = calcNumIterations( M )
-            r = size( M, 3);
-        end     
+    function r = getFinalScoreMatrix(this)
+%             disp('SSLMC::getFinalScoreMatrix');
+        r = this.m_Y(:,:,end);
+    end
 
-    end % methods ((Static))
+    %% bestScore
+    
+    function r = bestScore(this)
+        scoreMatrix = this.getFinalScoreMatrix();
+        [~,indices] = max(scoreMatrix,[],2);
+        bestScore = zeros(this.numVertices(), 1);
+        for vertex_i=1:length(bestScore)
+            bestScore( vertex_i ) = scoreMatrix( vertex_i, indices(vertex_i) );
+        end
+        r = bestScore;
+    end
+
+    %% binaryPrediction
+    
+    function r = binaryPrediction(this)
+        assert( this.numLabels() == (this.BINARY_NUM_LABELS) );
+        scoreMatrix = this.getFinalScoreMatrix();
+        [~,indices] = max(scoreMatrix,[],2);
+        scoreMatrix(:,this.NEGATIVE) = -scoreMatrix(:,this.NEGATIVE);
+        prediction = zeros(this.numVertices(), 1);
+        for vertex_i=1:length(prediction)
+            prediction( vertex_i ) = scoreMatrix( vertex_i, indices(vertex_i) );
+        end
+        r = prediction;
+    end
+
+    %% numVertices
+    
+    function r = numVertices(this)
+        r = SSLMC_Result.calcVertices(this.m_Y);
+    end
+
+    %% numIterations
+    
+    function r = numIterations(this)
+        r = SSLMC_Result.calcNumIterations(this.m_Y);
+    end
+
+    %% numLabels
+    
+    function r = numLabels(this)
+        r = SSLMC_Result.calcNumLabels( this.getFinalScoreMatrix() );
+    end
+
+end % methods (Access = public )
+
+methods (Static)
+
+    function r = calcVertices( M )
+            r = size( M, 1 );
+    end
+
+    function r = calcNumLabels( M )
+        r = size( M, 2 );
+    end
+
+    function r = calcNumIterations( M )
+        r = size( M, 3);
+    end     
+
+end % methods ((Static))
 end
