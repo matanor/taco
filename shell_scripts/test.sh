899336e552a98c5c669c6077d07c8413f8c01f69
matanorb <matanorb@4c94cbd0-a001-4ae1-9522-79914504ef8a>
2012-02-06 16:01:15 +0000

    Updated path of output, error and log files to be in the results directory.
    git-svn-id: https://matano1-tp.haifa.ibm.com/svn/taco@97 4c94cbd0-a001-4ae1-9522-79914504ef8a

--- shell_scripts/test.sh
+++ shell_scripts/test.sh
@@ -1,14 +1,19 @@
 
-runName=test
-outputFile=1.out
-errorFile=1.err
+outputDir=/u/matanorb/experiments/webkb/results
+folderName=2012_02_06_03_graph_heuristics_1
+runName=CSSLMC
+outputFile=$outputDir/$folderName/output.txt
+errorFile=$outputDir/$folderName/error.txt
+logFile=$outputDir/$folderName/matlab.log
 
+echo outputDir = $outputDir
+echo folderName = $folderName
 echo runName = $runName
 echo outputFile = $outputFile
 echo errorFile = $errorFile
 
-qsub -N $runName -wd /u/matanorb/matlab/Experiments -q all.q -b y -o $outputFile -e $errorFile "matlab -nodesktop -r "\""runOnOdin('2012_02_06')"\"" -logfile matlab.log"
-
+mkdir $outputDir/$folderName
+echo qsub -N $runName -wd /u/matanorb/matlab/Experiments -q all.q -b y -o $outputFile -e $errorFile "matlab -nodesktop -r "\""runOnOdin('$folderName')"\"" -logfile $logFile"
+qsub -N $runName -wd /u/matanorb/matlab/Experiments -q all.q -b y -o $outputFile -e $errorFile "matlab -nodesktop -r "\""runOnOdin('$folderName')"\"" -logfile $logFile"
 
 #qsub -N $runName -cwd -q all.q -pe matlablocal <number_of_cpu_needed> -b y -o $outputFile -e $errorFile "matlab -nodesktop -r "\""RunOnOdin"\"" -logfile matlab.log"
-#\'/u/matanorb/experiments/results/2012_02_06\'
