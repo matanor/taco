150109d0a02b3a16691bf0ba0e4f80f52e4ba0f2
matanorb <matanorb@4c94cbd0-a001-4ae1-9522-79914504ef8a>
2012-02-06 15:15:48 +0000

    First working version for odin.
    git-svn-id: https://matano1-tp.haifa.ibm.com/svn/taco@94 4c94cbd0-a001-4ae1-9522-79914504ef8a

--- shell_scripts/test.sh
+++ shell_scripts/test.sh
@@ -7,5 +7,8 @@ echo runName = $runName
 echo outputFile = $outputFile
 echo errorFile = $errorFile
 
-#qsub -N $runName -cwd -q all.q -pe matlablocal <number_of_cpu_needed> -b y -o $outputFile -e $errorFile "matlab -nodesktop -r "\""Test"\"" -logfile matlab.log"
-qsub -N $runName -cwd -q all.q -b y -o $outputFile -e $errorFile "matlab -nodesktop -r "\""Test"\"" -logfile matlab.log"
+qsub -N $runName -wd /u/matanorb/matlab/Experiments -q all.q -b y -o $outputFile -e $errorFile "matlab -nodesktop -r "\""runOnOdin('/u/matanorb/experiments/results/2012_02_06\')"\"" -logfile matlab.log"
+
+
+#qsub -N $runName -cwd -q all.q -pe matlablocal <number_of_cpu_needed> -b y -o $outputFile -e $errorFile "matlab -nodesktop -r "\""RunOnOdin"\"" -logfile matlab.log"
+#\'/u/matanorb/experiments/results/2012_02_06\'
