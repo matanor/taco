d4b0ed2f2985e24b4fda7332d601a790fdbb4715
matanorb <matanorb@4c94cbd0-a001-4ae1-9522-79914504ef8a>
2012-02-06 15:32:56 +0000

    Fixed folder name.
    git-svn-id: https://matano1-tp.haifa.ibm.com/svn/taco@96 4c94cbd0-a001-4ae1-9522-79914504ef8a

--- shell_scripts/test.sh
+++ shell_scripts/test.sh
@@ -7,7 +7,7 @@ echo runName = $runName
 echo outputFile = $outputFile
 echo errorFile = $errorFile
 
-qsub -N $runName -wd /u/matanorb/matlab/Experiments -q all.q -b y -o $outputFile -e $errorFile "matlab -nodesktop -r "\""runOnOdin('/u/matanorb/experiments/results/2012_02_06\')"\"" -logfile matlab.log"
+qsub -N $runName -wd /u/matanorb/matlab/Experiments -q all.q -b y -o $outputFile -e $errorFile "matlab -nodesktop -r "\""runOnOdin('2012_02_06')"\"" -logfile matlab.log"
 
 
 #qsub -N $runName -cwd -q all.q -pe matlablocal <number_of_cpu_needed> -b y -o $outputFile -e $errorFile "matlab -nodesktop -r "\""RunOnOdin"\"" -logfile matlab.log"
