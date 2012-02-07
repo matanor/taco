63fac1cb27a8a814c340869a568743cc699761c8
matanorb <matanorb@4c94cbd0-a001-4ae1-9522-79914504ef8a>
2012-02-07 22:08:13 +0000

    added debug prints.
    git-svn-id: https://matano1-tp.haifa.ibm.com/svn/taco@121 4c94cbd0-a001-4ae1-9522-79914504ef8a

--- Experiments/asyncSingleRun.m
+++ Experiments/asyncSingleRun.m
@@ -15,6 +15,8 @@ function asyncSingleRun(fileName, codeRoot)
     graph = GraphLoader.constructGraph( singleRunFactory.m_constructionParams );
     singleRunFactory.m_graph.w_nn           = graph.w_nn;
     singleRunFactory.m_graph.w_nn_symetric  = graph.w_nn_symetric;
+    disp(['size(graph.w_nn)_ = '          num2str(size(graph.w_nn))]);
+    disp(['size(graph.w_nn_symetric)_ = ' num2str(size(graph.w_nn_symetric))]);
     
     singleRun = singleRunFactory.run( runData.algorithmParams, runData.algorithmsToRun );
     
