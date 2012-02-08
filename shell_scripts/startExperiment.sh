001c5a50ec0c89d8108f70a3272467028e0ddf5e
matanorb <matanorb@4c94cbd0-a001-4ae1-9522-79914504ef8a>
2012-02-08 09:00:02 +0000

    renamed test.sh to startExperiment.sh. updated script to run matlab directly and not via qsub. We need this because we can not create child jobs not from the front end servers.
    git-svn-id: https://matano1-tp.haifa.ibm.com/svn/taco@125 4c94cbd0-a001-4ae1-9522-79914504ef8a

---
+++ shell_scripts/startExperiment.sh
@@ -0,0 +1,30 @@
+
+outputDir=/u/matanorb/experiments/webkb/results
+folderName=2012_02_07_05_async
+runName=CSSLMC
+outputFile=$outputDir/$folderName/output.txt
+errorFile=$outputDir/$folderName/error.txt
+logFile=$outputDir/$folderName/matlab.log
+codeRoot=/u/matanorb/matlab_async
+workingDirectory=$codeRoot/Experiments
+startDirectory="$(pwd)"
+
+echo codeRoot = $codeRoot
+echo workingDirectory = $workingDirectory
+echo startDirectory = $startDirectory
+echo outputDir = $outputDir
+echo folderName = $folderName
+echo runName = $runName
+echo outputFile = $outputFile
+echo errorFile = $errorFile
+
+mkdir $outputDir/$folderName
+cd $workingDirectory
+echo currentDirectory = "$(pwd)"
+echo "matlab -nodesktop -r "\""runOnOdin('$folderName','$codeRoot')"\"" "
+matlab -nodesktop -r "runOnOdin('$folderName','$codeRoot')" -logfile $logFile
+cd $startDirectory
+
+#echo qsub -N $runName -wd $codeRoot/Experiments -q all.q -b y -o $outputFile -e $errorFile "matlab -nodesktop -r "\""runOnOdin('$folderName','$codeRoot')"\"" -logfile $logFile"
+#qsub -N $runName -wd $codeRoot/Experiments -q all.q -b y -o $outputFile -e $errorFile "matlab -nodesktop -r "\""runOnOdin('$folderName','$codeRoot')"\"" -logfile $logFile"
+#qsub -N $runName -cwd -q all.q -pe matlablocal <number_of_cpu_needed> -b y -o $outputFile -e $errorFile "matlab -nodesktop -r "\""RunOnOdin"\"" -logfile matlab.log"
