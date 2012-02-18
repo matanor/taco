ed2e089acf5f9fc3d4f43da076a859326507682e
matanorb <matanorb@4c94cbd0-a001-4ae1-9522-79914504ef8a>
2012-02-18 17:25:16 +0000

    Moved call to Configurations.clearEverything to after setting the code path.
    git-svn-id: https://matano1-tp.haifa.ibm.com/svn/taco@189 4c94cbd0-a001-4ae1-9522-79914504ef8a

--- Experiments/runOnOdin.m
+++ Experiments/runOnOdin.m
@@ -2,11 +2,11 @@ function runOnOdin(folderName, codeRoot)
 %RUNONODIN Summary of this function goes here
 %   Detailed explanation goes here
     disp('runOnOdin');
-    Configurations.clearEverything();
-
     disp(['codeRoot = ' codeRoot]);
     codeFolders = genpath(codeRoot);
     addpath(codeFolders);
+    
+    Configurations.clearEverything();
 
     resultsDir = '/u/matanorb/experiments/webkb/results/';
     isOnOdin = 1;
