7877b4010ad20b0d3f4da475a731e6e0cb1eed27
matanorb <matanorb@4c94cbd0-a001-4ae1-9522-79914504ef8a>
2012-05-17 13:46:21 +0000

    startFunction.sh: based on startExperiment.sh (same directory). Takes as parameter the name of the function to run. startParallelKNN.sh: start parallel calculation of KNN.
    git-svn-id: https://matano1-tp.haifa.ibm.com/svn/taco@345 4c94cbd0-a001-4ae1-9522-79914504ef8a

---
+++ shell_scripts/startFunction.sh
@@ -0,0 +1,23 @@
+
+outputDir=/u/matanorb/experiments/results/
+folderName=2012_04_05_01_sentiment_no_random_seed_fixed_labeled_selection
+resultsDirectory=$outputDir$folderName
+logFile=$resultsDirectory/matlab.log
+codeRoot=/u/matanorb/matlab_async
+workingDirectory=$codeRoot/Experiments
+startDirectory="$(pwd)"
+
+echo codeRoot = $codeRoot
+echo workingDirectory = $workingDirectory
+echo startDirectory = $startDirectory
+echo outputDir = $outputDir
+echo resultsDirectory = $resultsDirectory
+echo folderName = $folderName
+echo logFile = $logFile
+
+mkdir $resultsDirectory
+cd $workingDirectory
+echo currentDirectory = "$(pwd)"
+echo "matlab -nodesktop -r "\""$1('$resultsDirectory','$codeRoot')"\"" "
+matlab -nodesktop -r "$1('$resultsDirectory','$codeRoot');quit;" -logfile $logFile
+cd $startDirectory
