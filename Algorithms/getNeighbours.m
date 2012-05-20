fa4b12a82ac87c22bca9da7342de6df37ac0efbf
matanorb <matanorb@4c94cbd0-a001-4ae1-9522-79914504ef8a>
2012-05-20 13:09:04 +0000

    Updated to get neighbors from the weight matrix columns instead of rows. This does not change the results singe the weight matrix is symmetric. Performance improves since matrices are ordered in memory by columns (especially for sparse)
    git-svn-id: https://matano1-tp.haifa.ibm.com/svn/taco@364 4c94cbd0-a001-4ae1-9522-79914504ef8a

--- Algorithms/getNeighbours.m
+++ Algorithms/getNeighbours.m
@@ -1,11 +1,6 @@
 function n = getNeighbours( W, vertex_i )
-%GETNEIGHBOURS Get the neighbours of a specific vertex.
-%   Detailed explanation goes here
-        
-    neighbours = find( W(vertex_i, :) ~= 0 );
-    neighbours_w = W(vertex_i, neighbours);
-    neighbours_w = neighbours_w.'; % make column vector
-
+    col = W(:, vertex_i);
+    [neighbours, ~, neighbours_w] = find(col);
     n.indices = neighbours;
     n.weights = neighbours_w;
 end
