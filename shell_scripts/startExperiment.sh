11a29647a40cd8a39b28ae3f0de9dea22b82943e
matanorb <matanorb@4c94cbd0-a001-4ae1-9522-79914504ef8a>
2012-05-23 06:27:15 +0000

    most code moved to startFunction.sh script. changed to use startFunction.sh script.
    git-svn-id: https://matano1-tp.haifa.ibm.com/svn/taco@383 4c94cbd0-a001-4ae1-9522-79914504ef8a

--- shell_scripts/startExperiment.sh
+++ shell_scripts/startExperiment.sh
@@ -1,23 +1,3 @@
+folderName=2012_05_23_01_structured_test_no_iterations
 
-outputDir=/u/matanorb/experiments/results/
-folderName=2012_04_05_01_sentiment_no_random_seed_fixed_labeled_selection
-resultsDirectory=$outputDir$folderName
-logFile=$resultsDirectory/matlab.log
-codeRoot=/u/matanorb/matlab_async
-workingDirectory=$codeRoot/Experiments
-startDirectory="$(pwd)"
-
-echo codeRoot = $codeRoot
-echo workingDirectory = $workingDirectory
-echo startDirectory = $startDirectory
-echo outputDir = $outputDir
-echo resultsDirectory = $resultsDirectory
-echo folderName = $folderName
-echo logFile = $logFile
-
-mkdir $resultsDirectory
-cd $workingDirectory
-echo currentDirectory = "$(pwd)"
-echo "matlab -nodesktop -r "\""runOnOdin('$resultsDirectory','$codeRoot')"\"" "
-matlab -nodesktop -r "runOnOdin('$resultsDirectory','$codeRoot');quit;" -logfile $logFile
-cd $startDirectory
+sh ./startFunction.sh runOnOdin $folderName
