2f8840ff78a5a00136328ab1dc4ea69f1fde1678
matanorb <matanorb@4c94cbd0-a001-4ae1-9522-79914504ef8a>
2012-05-25 22:39:18 +0000

    updated createInstancesWithContext() to use segments (where structured sentences start and end)
    git-svn-id: https://matano1-tp.haifa.ibm.com/svn/taco@401 4c94cbd0-a001-4ae1-9522-79914504ef8a

--- StructuredGenerator.m
+++ StructuredGenerator.m
@@ -46,52 +46,64 @@ methods
 end % methods
 
 methods (Static)
-    function main()
-        s = StructuredGenerator;
-        s.m_gaussianMean = [0 0;
-                            0 0];
-        s.m_gaussianCovariance(:,:,1) = [3 1.5;
-                                         1.5 1];
-        s.m_gaussianCovariance(:,:,2) = [3 -1.5;
-                                         -1.5 1];
-        s.m_transitions = [ 0.9 0.1 ;
-                            0.1 0.9 ];
-        s.m_prior = [0.5 0.5];
-        %dbstop in StructuredGenerator.m at 53;
-        [output states] = s.createSequence();
-        
-        class1 = output(states == 1, :);
-        class2 = output(states == 2, :);
-        hold on;
-        scatter(class1(:,1), class1(:,2) );
-        scatter(class2(:,1), class2(:,2) );
-        hold off;
-        
-        context = 1;
-        graph = s.createGraph(states, output, context);
-%         s.saveGraph( graph );
-        
-        context = 7;
-        graph = s.createGraph(states, output, context);
-%         s.saveGraph( graph );
-    end
-        
-    %% createInstancesWithContext
     
-    function R = createInstancesWithContext(instances, context)
-        numInstances    = size(instances, 1);
-        numFeatures     = size(instances, 2);
-        dummyContext = zeros(1, numFeatures) * 100; % some dummy value not likely to occur
-        instances_with_context = zeros(numInstances, context * numFeatures );
-        for instance_i=1:numInstances
-            if mod(instance_i,10000) == 0
-                Logger.log(['instances_i = ' num2str(instance_i)]);
+function main()
+    s = StructuredGenerator;
+    s.m_gaussianMean = [0 0;
+                        0 0];
+    s.m_gaussianCovariance(:,:,1) = [3 1.5;
+                                     1.5 1];
+    s.m_gaussianCovariance(:,:,2) = [3 -1.5;
+                                     -1.5 1];
+    s.m_transitions = [ 0.9 0.1 ;
+                        0.1 0.9 ];
+    s.m_prior = [0.5 0.5];
+    %dbstop in StructuredGenerator.m at 53;
+    [output states] = s.createSequence();
+
+    class1 = output(states == 1, :);
+    class2 = output(states == 2, :);
+    hold on;
+    scatter(class1(:,1), class1(:,2) );
+    scatter(class2(:,1), class2(:,2) );
+    hold off;
+
+    context = 1;
+    graph = s.createGraph(states, output, context);
+%         s.saveGraph( graph );
+
+    context = 7;
+    graph = s.createGraph(states, output, context);
+%         s.saveGraph( graph );
+end
+
+%% createInstancesWithContext
+
+function R = createInstancesWithContext(instances, context, segments)
+    numInstances    = size(instances, 1);
+    numFeatures     = size(instances, 2);
+    dummyContext = ones(1, numFeatures) * 100; % some dummy value not likely to occur
+    instances_with_context = zeros(numInstances, context * numFeatures );
+    numSegments = size(segments, 1);
+    
+    Logger.log(['numInstances = ' num2str(numInstances)]);
+    Logger.log(['numFeatures = '  num2str(numFeatures)]);
+    Logger.log(['numSegments = '  num2str(numSegments)]);
+    
+    instanceCount = 1;
+    for segment_i=1:numSegments
+        segmentStart = segments(segment_i,1);
+        segmentEnd   = segments(segment_i,2);
+        segmentFramesRange = segmentStart:segmentEnd;
+        for instance_i=segmentFramesRange
+            if mod(instanceCount,10000) == 0
+                Logger.log(['instanceCount = ' num2str(instanceCount)]);
             end
             half_context = (context - 1)/2;
             context_range = (instance_i-half_context):(instance_i+half_context);
             contextPosition = 1:numFeatures;
             for context_i=context_range
-                if context_i < 1 || context_i > numInstances
+                if context_i < segmentStart || context_i > segmentEnd
                     contextInstance = dummyContext;
                 else
                     contextInstance = instances(context_i, :);
@@ -99,131 +111,135 @@ methods (Static)
                 instances_with_context(instance_i, contextPosition) = contextInstance;
                 contextPosition = contextPosition + numFeatures;
             end
+            instanceCount = instanceCount + 1;
         end
-        R = instances_with_context;
     end
-    
-    %% createGraph
-    
-    function graph = createGraph( labels, instances, context )
-        graph.labels = labels;
-        graph.instances = instances;
-        numInstances    = size(instances, 1);
-        instances_with_context = ...
-            StructuredGenerator.createInstancesWithContext(instances, context );
-        weights = zeros(numInstances, numInstances);
-        alpha = 2;
-        for instance_i=1:numInstances
-            x = instances_with_context(instance_i,:);
-            for instance_j=1:numInstances
-                y = instances_with_context(instance_j,:);
-                weights(instance_i, instance_j) = exp(-sum((x-y).^2) / alpha);
-            end
+    R = instances_with_context;
+end
+
+%% createGraph
+
+function graph = createGraph( labels, instances, context )
+    graph.labels = labels;
+    graph.instances = instances;
+    numInstances    = size(instances, 1);
+    segments = [1 numInstances];
+    instances_with_context = ...
+        StructuredGenerator.createInstancesWithContext...
+                            (instances, context, segments );
+    weights = zeros(numInstances, numInstances);
+    alpha = 2;
+    for instance_i=1:numInstances
+        x = instances_with_context(instance_i,:);
+        for instance_j=1:numInstances
+            y = instances_with_context(instance_j,:);
+            weights(instance_i, instance_j) = exp(-sum((x-y).^2) / alpha);
         end
-        graph.weights = weights;
-        graph.name = ['context_' num2str(context)];
     end
-    
-    %% segmentsToStructuredEdges
-    
-    function R = segmentsToStructuredEdges(segments)
-        lastSeg = segments(end,end);
-        structuredEdges = [(1:lastSeg-1).' (2:lastSeg).'];
-        segmentsEnd = segments(:,2);
-        segmentsEnd = segmentsEnd(1:end-1); % remove last segment end
-        structuredEdges(segmentsEnd,:) = [];  % remove edges from segment end to a different segment start
-        R = structuredEdges;
-    end
-    
-    %% estimateTransitionMatrix
-    
-    function R = estimateTransitionMatrix(correctLabels, segments)
-        numLabels = length(unique(correctLabels));
-        Logger.log(['Number of classes = ' num2str(numLabels)]);
-        transitions = zeros(numLabels, numLabels);
-        numSegments = size(segments, 1);
-        Logger.log(['Number of segments = ' num2str(numSegments)]);
-        Logger.log(['Labels sequence length = ' num2str(length(correctLabels))]);
-        numTransitions = 0;
-        for segment_i=1:numSegments
-            segmentStart = segments(segment_i,1);
-            segmentEnd   = segments(segment_i,2);
-            for frame_i=segmentStart:(segmentEnd-1)
-                framePhone      = correctLabels(frame_i);
-                nextFramePhone  = correctLabels(frame_i+1);
-                transitions(nextFramePhone, framePhone) = ...
-                    transitions(nextFramePhone, framePhone) + 1;
-                numTransitions = numTransitions +1;
-            end
+    graph.weights = weights;
+    graph.name = ['context_' num2str(context)];
+end
+
+%% segmentsToStructuredEdges
+
+function R = segmentsToStructuredEdges(segments)
+    lastSeg = segments(end,end);
+    structuredEdges = [(1:lastSeg-1).' (2:lastSeg).'];
+    segmentsEnd = segments(:,2);
+    segmentsEnd = segmentsEnd(1:end-1); % remove last segment end
+    structuredEdges(segmentsEnd,:) = [];  % remove edges from segment end to a different segment start
+    R = structuredEdges;
+end
+
+%% estimateTransitionMatrix
+
+function R = estimateTransitionMatrix(correctLabels, segments)
+    numLabels = length(unique(correctLabels));
+    Logger.log(['Number of classes = ' num2str(numLabels)]);
+    transitions = zeros(numLabels, numLabels);
+    numSegments = size(segments, 1);
+    Logger.log(['Number of segments = ' num2str(numSegments)]);
+    Logger.log(['Labels sequence length = ' num2str(length(correctLabels))]);
+    numTransitions = 0;
+    for segment_i=1:numSegments
+        segmentStart = segments(segment_i,1);
+        segmentEnd   = segments(segment_i,2);
+        for frame_i=segmentStart:(segmentEnd-1)
+            framePhone      = correctLabels(frame_i);
+            nextFramePhone  = correctLabels(frame_i+1);
+            transitions(nextFramePhone, framePhone) = ...
+                transitions(nextFramePhone, framePhone) + 1;
+            numTransitions = numTransitions +1;
         end
-        Logger.log(['Number of transitions = ' num2str(numTransitions)]);
-        R = transitions ./ repmat(sum(transitions, 1), numLabels, 1);
     end
-    
-    %% sampleSegments
-    
-    function R = sampleSegments(correctLabels, segments, precentToSample)
-        numLabels = length(unique(correctLabels));
-        minNumSamplesPerLabel = 1;
-        numSampledFromEachLabel = zeros(numLabels, 1);
-        numSegments = size(segments, 1);
-        numFrames = length(correctLabels);
-        Logger.log(['numLabels = ' num2str(numLabels)]);
-        Logger.log(['numSegments = ' num2str(numSegments)]);
-        Logger.log(['numFrames = '   num2str(numFrames)]);
-        allSampledSegments = [];
-        numLabeledSamples = 0;
-        labelsSampleOrder = randperm(numLabels);
-        for label_i=labelsSampleOrder
-            finished = (numSampledFromEachLabel(label_i) >= minNumSamplesPerLabel);
-            while ~finished
-                sampledSegment = randi(numSegments, 1, 1);
-                if ~ismember(sampledSegment, allSampledSegments)
-                    segmentStart = segments(sampledSegment, 1);
-                    segmentEnd   = segments(sampledSegment, 2);
-                    labelsInSegment = correctLabels(segmentStart:segmentEnd);
-                    if ismember(label_i, labelsInSegment)
-                        finished = 1;
-                        allSampledSegments = [allSampledSegments; sampledSegment]; %#ok<AGROW>
-                        Logger.log(['Adding segment ' num2str(sampledSegment)]);
-                        numLabelsInSegment = length(labelsInSegment);
-                        numLabeledSamples = numLabeledSamples + numLabelsInSegment;
-                        for labelInSegment_i=labelsInSegment
-                            numSampledFromEachLabel(labelInSegment_i) = ...
-                                numSampledFromEachLabel(labelInSegment_i) + 1;
-                        end
-                    end
-                end
-            end
-        end
-        
-        finished = numLabeledSamples > precentToSample * numFrames;
-        
+    Logger.log(['Number of transitions = ' num2str(numTransitions)]);
+    R = transitions ./ repmat(sum(transitions, 1), numLabels, 1);
+end
+
+%% sampleSegments
+
+function R = sampleSegments(correctLabels, segments, precentToSample)
+    numLabels = length(unique(correctLabels));
+    minNumSamplesPerLabel = 1;
+    numSampledFromEachLabel = zeros(numLabels, 1);
+    numSegments = size(segments, 1);
+    numFrames = length(correctLabels);
+    Logger.log(['numLabels = ' num2str(numLabels)]);
+    Logger.log(['numSegments = ' num2str(numSegments)]);
+    Logger.log(['numFrames = '   num2str(numFrames)]);
+    allSampledSegments = [];
+    numLabeledSamples = 0;
+    labelsSampleOrder = randperm(numLabels);
+    for label_i=labelsSampleOrder
+        finished = (numSampledFromEachLabel(label_i) >= minNumSamplesPerLabel);
         while ~finished
             sampledSegment = randi(numSegments, 1, 1);
             if ~ismember(sampledSegment, allSampledSegments)
                 segmentStart = segments(sampledSegment, 1);
                 segmentEnd   = segments(sampledSegment, 2);
                 labelsInSegment = correctLabels(segmentStart:segmentEnd);
-                allSampledSegments = [allSampledSegments; sampledSegment]; %#ok<AGROW>
-                numLabeledSamples = numLabeledSamples + length(labelsInSegment);
+                if ismember(label_i, labelsInSegment)
+                    finished = 1;
+                    allSampledSegments = [allSampledSegments; sampledSegment]; %#ok<AGROW>
+                    Logger.log(['Adding segment ' num2str(sampledSegment)]);
+                    numLabelsInSegment = length(labelsInSegment);
+                    numLabeledSamples = numLabeledSamples + numLabelsInSegment;
+                    for labelInSegment_i=labelsInSegment
+                        numSampledFromEachLabel(labelInSegment_i) = ...
+                            numSampledFromEachLabel(labelInSegment_i) + 1;
+                    end
+                end
             end
-            finished = numLabeledSamples > precentToSample * numFrames;
         end
-        
-        assert(length(unique(allSampledSegments)) == length(allSampledSegments));
-        
-        sampledLabels = [];
-        for segment_i=allSampledSegments.'
-            segmentStart = segments(segment_i, 1);
-            segmentEnd   = segments(segment_i, 2);
-            sampledLabels = [sampledLabels; (segmentStart:segmentEnd).']; %#ok<AGROW>
-        end
-        
-        R = sampledLabels;
     end
-    
+
+    finished = numLabeledSamples > precentToSample * numFrames;
+
+    while ~finished
+        sampledSegment = randi(numSegments, 1, 1);
+        if ~ismember(sampledSegment, allSampledSegments)
+            segmentStart = segments(sampledSegment, 1);
+            segmentEnd   = segments(sampledSegment, 2);
+            labelsInSegment = correctLabels(segmentStart:segmentEnd);
+            allSampledSegments = [allSampledSegments; sampledSegment]; %#ok<AGROW>
+            numLabeledSamples = numLabeledSamples + length(labelsInSegment);
+        end
+        finished = numLabeledSamples > precentToSample * numFrames;
+    end
+
+    assert(length(unique(allSampledSegments)) == length(allSampledSegments));
+
+    sampledLabels = [];
+    for segment_i=allSampledSegments.'
+        segmentStart = segments(segment_i, 1);
+        segmentEnd   = segments(segment_i, 2);
+        sampledLabels = [sampledLabels; (segmentStart:segmentEnd).']; %#ok<AGROW>
+    end
+
+    R = sampledLabels;
 end
+
+end % methods (Static)
     
 end
 
