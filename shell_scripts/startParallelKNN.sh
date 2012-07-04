7f2af7b3a366e3e27a9b34206df65e35fe015f86
matanorb <matanorb@4c94cbd0-a001-4ae1-9522-79914504ef8a>
2012-07-04 14:55:46 +0000

    Removed the no longer required parameter codeRoot from the call to the startFunction.sh script.
    git-svn-id: https://matano1-tp.haifa.ibm.com/svn/taco@460 4c94cbd0-a001-4ae1-9522-79914504ef8a

--- shell_scripts/startParallelKNN.sh
+++ shell_scripts/startParallelKNN.sh
@@ -1,4 +1,3 @@
 folderName=2012_05_20_01_construct_KNN
-codeRoot=/u/matanorb/matlab_async
 
-sh ./startFunction.sh runParallelKNN $folderName $codeRoot
+sh ./startFunction.sh runParallelKNN $folderName
