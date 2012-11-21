20ca6f539a74387b22610da3fffd67446fc746ff
matanorb <matanorb@4c94cbd0-a001-4ae1-9522-79914504ef8a>
2012-11-21 09:03:10 +0000

    plotPhonemeMedians.m:
    added code to plot histogram of median 10th nearest neighbor, per each class.
    added code to histogram distance to 5th neighbor (median of 10 nearest neighbors which is just the fifth).
    
    git-svn-id: https://matano1-tp.haifa.ibm.com/svn/taco@539 4c94cbd0-a001-4ae1-9522-79914504ef8a

--- plotPhonemeMedians.m
+++ plotPhonemeMedians.m
@@ -1,12 +1,24 @@
-rootFolder = 'C:\technion\theses\Experiments\timit\features_39\trainAndTest';
+rootFolder = 'C:\technion\theses\Experiments\timit\features_39_cms_white\trainAndTest';
 
-distancesFile = [rootFolder '\trainAndTest_notWhite.context7.k_10.mat'];
-featuresFile  = [rootFolder '\trainAndTest_notWhite.context7.mat'];
+distancesFile = [rootFolder '\trainAndTest_cms_white.context7.k_10.mat'];
+featuresFile  = [rootFolder '\trainAndTest_cms_white.context7.mat'];
 
+%% 
 load(distancesFile);
 k10 = max(graph.distances,[],2);
+graph.distances = graph.distances.';
+numCols = size(graph.distances,2);
+med = zeros(numCols,1);
+for col_i=1:numCols
+    if mod(col_i, 10000) == 0
+        Logger.log(['col_i = ' num2str(col_i)]);
+    end
+    [~, ~, col_values] = find(graph.distances(:,col_i));
+    med(col_i) = sqrt(median(col_values));
+end
 clear graph;
 
+%% 
 load(featuresFile);
 trueLabel = graph.phoneids39;
 clear graph;
@@ -16,3 +28,16 @@ for class_i=1:39
     classMedian(class_i) = median(sqrt(x));
     classMean(class_i) = mean(sqrt(x));
 end
+
+%%
+
+figure('name','histogram of median of 10 th neighbour, per class');
+hist(classMedian);
+
+%%
+
+figure('name','median of 10 nearest neighbours histogram');
+hist(med,50);
+yL = get(gca,'YLim');
+%line([3 3],yL,'Color','r');
+
