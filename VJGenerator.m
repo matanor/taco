830f66ae012434c48aff0e3ddf42c270f6e44c25
matanorb <matanorb@4c94cbd0-a001-4ae1-9522-79914504ef8a>
2012-11-28 22:56:08 +0000

    added generateTrunsductionSets_main(), generateTrunsductionSets(), createExperimentTransductionSets(), createTransductionSet() updated vjFormatToMatlab() to save original text file in matlab format
    git-svn-id: https://matano1-tp.haifa.ibm.com/svn/taco@575 4c94cbd0-a001-4ae1-9522-79914504ef8a

--- VJGenerator.m
+++ VJGenerator.m
@@ -7,16 +7,88 @@ end
 
 methods  (Static)  
 
+    %% generateTrunsductionSets_main
+    
+    function generateTrunsductionSets_main()
+        allFileIdentifiers{1} = 'v4.w1';
+        allFileIdentifiers{2} = 'v4.w7';
+        allFileIdentifiers{3} = 'v8.w1';
+        allFileIdentifiers{4} = 'v8.w7';
+        num_files = length(allFileIdentifiers);
+        for file_i = 1:num_files
+            fileIdentifier = allFileIdentifiers{file_i};
+            VJGenerator.generateTrunsductionSets(fileIdentifier);
+        end
+    end
+    
+    %% generateTrunsductionSets
+    
+    function generateTrunsductionSets(fileIdentifier)
+        rootDirectory = 'c:/technion/theses/experiments/VJ/';
+        
+        fileExtension = '.mat';
+        trainFullPath = [rootDirectory 'train.' fileIdentifier fileExtension];
+        devFullPath   = [rootDirectory 'dev.'   fileIdentifier fileExtension];
+        testFullPath  = [rootDirectory 'test.'  fileIdentifier fileExtension];
+        train = load(trainFullPath);
+        dev   = load(devFullPath);
+        test  = load(testFullPath);
+        numTrain = length(train.labels);
+        numDev   = length(dev.labels);
+        numTest  = length(test.labels);
+        
+        precentLabeledToSample = [1 5 10 20 30 50];
+        for precent_labeled_i=precentLabeledToSample
+            numLabeled = floor(precent_labeled_i * numTrain / 100);
+            trunsductionSets  = VJGenerator.createExperimentTransductionSets...
+                                (numTrain, numDev, numTest, numLabeled); %#ok<NASGU>
+            trunsductionSetsOutputFullPath = ...
+                 [rootDirectory fileIdentifier '.TrunsSet_' num2str(numLabeled) '.mat'];
+            Logger.log(['Saving to file ''' trunsductionSetsOutputFullPath '''']);
+            save(trunsductionSetsOutputFullPath,'trunsductionSets');
+        end
+    end
+    
+    %% createExperimentTransductionSets
+    
+    function R = createExperimentTransductionSets(numTrain, numDev, numTest, numLabeled)
+        t = ExperimentTrunsductionSets();
+        Logger.log(['VJGenerator::createExperimentTransductionSets.' ...
+                    ' numTrain = '      num2str(numTrain) ...
+                    ' numDev = '        num2str(numDev) ...
+                    ' numTest = '       num2str(numTest) ...
+                    ' numLabeled = '    num2str(numLabeled) ...
+                    ]);
+        t.m_optimizationSets = VJGenerator.createTransductionSet(numTrain, numDev,  numLabeled);
+        t.m_evaluationSets   = VJGenerator.createTransductionSet(numTrain, numTest, numLabeled);
+        
+        R = t;
+    end
+    
+        %% createTransductionSet
+    
+    function R = createTransductionSet(numTrain, numTest, numLabeled)
+        t = TrunsductionSet(1);
+        % all are column vectors
+        t.m_training        = (1:numTrain).';
+        t.m_testing         = numTrain + ((1:numTest).');
+        randomPermutation   = randperm(numTrain);
+        sampled             = (randomPermutation(1:numLabeled)).';
+        t.m_labeled         = sampled;
+        
+        R = t;
+    end
+    
     %% createWeightsFromDistances_lihi_main
     
     function createWeightsFromDistances_lihi_main()
         rootDirectoty = 'c:/technion/theses/experiments/VJ/';
         allFilePrefixes{1} = [rootDirectoty 'trainAndDev/trainAndDev.instances.v4.w1'];
         allFilePrefixes{2} = [rootDirectoty 'trainAndDev/trainAndDev.instances.v4.w7'];
-        allFilePrefixes{3} = [rootDirectoty 'trainAndDev/trainAndDev.instances.v8.w7'];
+        allFilePrefixes{3} = [rootDirectoty 'trainAndDev/trainAndDev.instances.v8.w1'];
         allFilePrefixes{4} = [rootDirectoty 'trainAndTest/trainAndTest.instances.v4.w1'];
         allFilePrefixes{5} = [rootDirectoty 'trainAndTest/trainAndTest.instances.v4.w7'];
-        allFilePrefixes{6} = [rootDirectoty 'trainAndTest/trainAndTest.instances.v8.w7'];
+        allFilePrefixes{6} = [rootDirectoty 'trainAndTest/trainAndTest.instances.v8.w1'];
 %         allFilePrefixes{7} = [rootDirectoty 'trainAndDev/trainAndDev.instances.v8.w7'];
 %         allFilePrefixes{8} = [rootDirectoty 'trainAndTest/trainAndTest.instances.v8.w7'];
         num_files = length(allFilePrefixes);
@@ -54,13 +126,14 @@ methods  (Static)
     
     function vjFormatToMatlab( inputDirectory, fileIdentifier )
         TRAIN = 1; DEV = 2; TEST = 3;
-        inputFilesFullPath{TRAIN} = [inputDirectory 'train.' fileIdentifier '.libsvm.dat.matlab1'];
-        inputFilesFullPath{DEV}   = [inputDirectory 'dev.'   fileIdentifier '.libsvm.dat.matlab1'];
-        inputFilesFullPath{TEST}  = [inputDirectory 'test.'  fileIdentifier '.libsvm.dat.matlab1'];
-        num_files = length(inputFilesFullPath);
+        allInputFilesFullPath{TRAIN} = [inputDirectory 'train.' fileIdentifier '.libsvm.dat.matlab1'];
+        allInputFilesFullPath{DEV}   = [inputDirectory 'dev.'   fileIdentifier '.libsvm.dat.matlab1'];
+        allInputFilesFullPath{TEST}  = [inputDirectory 'test.'  fileIdentifier '.libsvm.dat.matlab1'];
+        num_files = length(allInputFilesFullPath);
         for file_i = 1:num_files
-            Logger.log(['sparseKnn::vjFormatToMatlab. loading = ''' inputFilesFullPath{file_i} '''']);
-            data = load(inputFilesFullPath{file_i}); 
+            inputFileFullPath = allInputFilesFullPath{file_i};
+            Logger.log(['sparseKnn::vjFormatToMatlab. loading = ''' inputFileFullPath '''']);
+            data = load(inputFileFullPath); 
             labels = data(:,1);
             features = data(:,3:2:end);
             num_instances = length(labels);
@@ -70,7 +143,13 @@ methods  (Static)
             assert(num_features == size(features, 2));
             allFeatures{file_i}.instances = features; %#ok<AGROW>
             allFeatures{file_i}.labels    = labels; %#ok<AGROW>
-            [~, name, ~] = fileparts(inputFilesFullPath{file_i});
+            [path, name, ~] = fileparts(inputFileFullPath);
+            % name is like 'dev.v4.w1.libsvm.dat.matlab1'
+            dots = strfind(name, '.');
+            name = name(1:(dots(3)-1));
+            outputFeaturesFullPath = [path '/' name '.mat'];
+            Logger.log(['sparseKnn::vjFormatToMatlab. outputFeaturesFullPath = ''' outputFeaturesFullPath  '''']);
+            save(outputFeaturesFullPath, 'features', 'labels')
         end
         
         outputFileFullPath = [ inputDirectory 'trainAndDev/trainAndDev.instances.' fileIdentifier '.mat'];
