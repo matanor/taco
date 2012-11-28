3cc3cc1588952b847311d5c6e7ad2f44199f98f2
matanorb <matanorb@4c94cbd0-a001-4ae1-9522-79914504ef8a>
2012-11-28 14:18:37 +0000

    updated runParallelKNN() to supprt multiple files in a single run
    git-svn-id: https://matano1-tp.haifa.ibm.com/svn/taco@570 4c94cbd0-a001-4ae1-9522-79914504ef8a

--- Experiments/runParallelKNN.m
+++ Experiments/runParallelKNN.m
@@ -3,12 +3,26 @@ function runParallelKNN(resultsDirectory, codeRoot)
 %   Detailed explanation goes here
     disp('runParallelKNN');
     outputManager = initRunOnOdin(resultsDirectory, codeRoot);
-    inputFileFullPath = '/u/matanorb/experiments/timit/trainAndTest.mat';
-    K = 10;
-    instancesPerJob = 500;
-%     maxInstances = 10 * instancesPerJob;
-    maxInstances = Inf;
-    sparseKnn.calcKnnMain(inputFileFullPath, K, instancesPerJob, ...
-                          maxInstances,      outputManager);
+    rootDirectoty = '/u/matanorb/experiments/VJ/';
+    allInputFiles{1} = [rootDirectoty 'trainAndDev/trainAndDev.instances.v4.w7.mat'];
+    allInputFiles{3} = [rootDirectoty 'trainAndTest/trainAndTest.instances.v4.w7.mat'];
+    allInputFiles{2} = [rootDirectoty 'trainAndDev/trainAndDev.instances.v8.w1.mat'];
+    allInputFiles{4} = [rootDirectoty 'trainAndTest/trainAndTest.instances.v8.w1.mat'];
+                     
+	num_files = length(allInputFiles);
+	for file_i=1:num_files
+        inputFileFullPath = allInputFiles{file_i};
+        Logger.log(['::runParallelKNN. inputFileFullPath = ''' inputFileFullPath '''']);
+        [~, folder, ~] = fileparts(inputFileFullPath);
+        Logger.log(['::runParallelKNN. output in subfolder = ''' folder '''']);
+        outputManager.stepIntoFolder(folder);
+        K = 10;
+        instancesPerJob = 500;
+    %     maxInstances = 10 * instancesPerJob;
+        maxInstances = Inf;
+        sparseKnn.calcKnnMain(inputFileFullPath, K, instancesPerJob, ...
+                              maxInstances,      outputManager);
+        outputManager.moveUpOneDirectory();
+    end
 end
 
