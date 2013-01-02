119a60a16a9848d543756969a622d552a369b28e
matanorb <matanorb@4c94cbd0-a001-4ae1-9522-79914504ef8a>
2013-01-02 14:24:00 +0000

    updated code generating ECML tables. This is in preparations for adding to thesis. ********************************************** updated convert(), createTables_ecml2012(), printOneDataset(), printLine(), metricToString(), startTable_ecml2012(), endTable_ecml2012(), trimValues()
    git-svn-id: https://matano1-tp.haifa.ibm.com/svn/taco@593 4c94cbd0-a001-4ae1-9522-79914504ef8a

--- Experiments/Reporting/ExcelToLatexConverter.m
+++ Experiments/Reporting/ExcelToLatexConverter.m
@@ -4,7 +4,7 @@ classdef ExcelToLatexConverter < TextReader
 
 %% file name on office desktop
 % For paper 2012_TACO_in_ECML
-% fileName = 'C:/technion/theses/Experiments/results/2012_04_22 For Paper Graph based transduction with Confidence/BigTableSummary.txt';    
+% fileName = 'C:/technion/theses/Experiments/results/2012_04_22_For_Paper_Graph_based_transduction_with_Confidence/BigTableSummary.txt';    
 % For paper 2012 TACO on speech EILAT IEEEI
 % fileName = 'C:/technion/theses/Experiments/results/2012_09_02_01 Speech Results Summary For Paper/BigTableSummary.txt'
 % fileName = 'e:/technion/theses/Experiments/results/2012_09_02_01 Speech Results Summary For Paper/BigTableSummary.txt'
@@ -37,10 +37,11 @@ methods (Access = public)
         this.close();
         this.trimHeaders();
         this.createMaps();
-        this.trimValues();
-        this.createGraphs_eilat_2012();
+        isTrimLevenshtein = 0;
+        this.trimValues(isTrimLevenshtein);
+        %this.createGraphs_eilat_2012();
         %this.createGraphs_ecml_2012();
-        %this.createTables();
+        this.createTables_ecml2012();
         %this.createWebKBTable();
 %         this.createMultipleDatasetGraphs();
         this.clearAll();
@@ -806,34 +807,58 @@ methods (Access = public)
         fprintf(figuresFileID,'}\n');
     end
     
-    %% createTables
+    %% createTables_ecml2012
     
-    function createTables(this)
-        graph.key = 'graph';
+    function createTables_ecml2012(this)
+        
         nlpGraphNames = this.nlpGraphNames();
         numLabeledPerGraph = this.numLabeledPerGraphForTables();
+        
+        graph.key = 'graph';
         graph.shouldMatch = 1;
+        
         balanced.key = 'balanced';
         balanced.value = {'0'};
         balanced.shouldMatch = 1;
-        num_labeled.key = 'num labeled';
-        
+
         num_iterations.key = 'max iterations';
         num_iterations.value = {'10'};
         num_iterations.shouldMatch = 1;
         
+        num_labeled.key = 'num labeled';
+        
+        
         num_labeled.shouldMatch = 1;
         labeled_init.key = 'labelled init';
-        
         labeled_init.shouldMatch = 1;
+        labeled_init.value = {'1'}; % Hoe data is initialized
+        
 %         optimize_by.key = 'optimize_by';
 %         optimize_by.shouldMatch = 1;
         
         table_i = 1;
         
         %optimize_by.value = { 'PRBEP' };
-        labeled_init.value = {'1'};
+
         searchProperties{table_i} = [balanced labeled_init num_iterations];
+        optimizeBy.leftColumn {table_i} = 'PRBEP';
+        optimizeBy.rightColumn{table_i} = 'macroACC';
+        optimizeBy.leftName{table_i}    = 'PRBEP';
+        optimizeBy.rightName{table_i}   = 'M-ACC';
+        table_i = table_i + 1;
+        
+        searchProperties{table_i} = [balanced labeled_init num_iterations];
+        optimizeBy.leftColumn {table_i} = 'accuracy';
+        optimizeBy.rightColumn{table_i} = 'MRR';
+        optimizeBy.leftName{table_i}    = 'accuracy';
+        optimizeBy.rightName{table_i}   = 'MRR';
+        table_i = table_i + 1;
+
+        searchProperties{table_i} = [balanced labeled_init num_iterations];
+        optimizeBy.leftColumn {table_i} = 'macroACC';
+        optimizeBy.rightColumn{table_i} = 'macroMRR';
+        optimizeBy.leftName{table_i}    = 'M-ACC';
+        optimizeBy.rightName{table_i}   = 'M-MRR';
         table_i = table_i + 1;
 
         %optimize_by.value = { 'macroACC' };
@@ -849,19 +874,25 @@ methods (Access = public)
 %         searchProperties{table_i} = [balanced num_labeled labeled_init optimize_by];
 %         table_i = table_i + 1;
 
-        outputFileName = [this.inputFileName() '.tex'];
+        outputFileName = [this.inputFileName() '.ecml2012.tex'];
         outputFileID = fopen(outputFileName, 'w+');
         
         for table_i=1:length(searchProperties)
-            this.startTable( outputFileID );
+            
+            Logger.log( ['createTables_ecml2012. table_i = ' num2str(table_i)] );
+            this.startTable_ecml2012( outputFileID, ...
+                                      optimizeBy.leftName{table_i}, ...
+                                      optimizeBy.rightName{table_i});
         
             for graph_i = 1:length(nlpGraphNames)
                 graph.value = nlpGraphNames(graph_i);
                 num_labeled.value = numLabeledPerGraph(graph_i);
                 this.printOneDataset(outputFileID, graph.value{1}, ...
-                    [num_labeled graph searchProperties{table_i}]);
+                    [num_labeled graph searchProperties{table_i}], ...
+                    optimizeBy.leftColumn{table_i}, ...
+                    optimizeBy.rightColumn{table_i});
             end
-            this.endTable( outputFileID, ...
+            this.endTable_ecml2012( outputFileID, ...
                 [num_labeled graph searchProperties{table_i}] );
         end
         fclose(outputFileID);
@@ -912,23 +943,23 @@ methods (Access = public)
     %% printOneDataset
     
     function printOneDataset(this, outputFile, dataSetName, ...
-                             searchProperties )
+                             searchProperties, optimizeByLeftColumn, optimizeByRightColumn )
         optimize_by.key = 'optimize_by';
         optimize_by.shouldMatch = 1;
         
         Logger.log( ['printOneDataset. data set name = ' dataSetName] );
 
-        optimize_by.value = { 'PRBEP' };
-        PRBEP = this.findAlgorithms([searchProperties optimize_by]);
+        optimize_by.value = { optimizeByLeftColumn }; %'PRBEP' };
+        leftColumnResult = this.findAlgorithms([searchProperties optimize_by]);
         
-        optimize_by.value = { 'macroACC' };
-        macroAcc = this.findAlgorithms([searchProperties optimize_by]);
+        optimize_by.value = { optimizeByRightColumn }; %'macroACC' };
+        rightColumnResults = this.findAlgorithms([searchProperties optimize_by]);
         
         trimPosition = find(~isletter(dataSetName));
         if (~isempty(trimPosition))
             dataSetName = dataSetName(1:(trimPosition-1));
         end
-        numLabeled = [PRBEP.diag('num labeled') ' labeled' ];
+        numLabeled = [leftColumnResult.diag('num labeled') ' labeled' ];
         
         lineFormat = ['%s    & ~%s~  & ~%s~ & ~%s~ & ~%s~ & ~%s~ & ~%s~ & ~%s~ \\\\ \\cline{%s-8}\n'];
 
@@ -938,77 +969,91 @@ methods (Access = public)
         %fprintf(outputFile, '\\\\ \\hline\n');
         fprintf(outputFile, '\\hline \\hline\n');
         this.printLine(outputFile, lineFormat, key, [], ...
-                        PRBEP, macroAcc, metricName, columnStartHorzLine );
+                        leftColumnResult, rightColumnResults, metricName, columnStartHorzLine );
         
         key = 'avg accuracy';    
         metricName = 'ACC';
         this.printLine(outputFile, lineFormat, key, ['\' dataSetName],...
-                        PRBEP, macroAcc, metricName, columnStartHorzLine );
+                        leftColumnResult, rightColumnResults, metricName, columnStartHorzLine );
         
         key = 'avg macro accuracy'; 
         metricName = 'M-ACC';
         this.printLine(outputFile, lineFormat, key, numLabeled, ...
-                        PRBEP, macroAcc, metricName, columnStartHorzLine );
+                        leftColumnResult, rightColumnResults, metricName, columnStartHorzLine );
         
         key = 'avg MRR'; 
         metricName = 'MRR';
         this.printLine(outputFile, lineFormat, key, [],...
-                        PRBEP, macroAcc, metricName, columnStartHorzLine );
+                        leftColumnResult, rightColumnResults, metricName, columnStartHorzLine );
         
         key = 'avg macro MRR'; 
         metricName = 'M-MRR';
         columnStartHorzLine = '1';
         this.printLine(outputFile, lineFormat, key, [],...
-                        PRBEP, macroAcc, metricName, columnStartHorzLine );
+                        leftColumnResult, rightColumnResults, metricName, columnStartHorzLine );
     end
     
     %% printLine
     
     function printLine(this, outputFile, lineFormat, ...
-                       key, linePrefix, PRBEP, macroAcc, metricName,...
-                       columnStartHorzLine)
-        optimizePRBEP_results    = this.metricToString(key, PRBEP);
-        optimizeMacroAcc_results = this.metricToString(key, macroAcc);
+                       key, linePrefix, leftMetricValues, rightMetricValues,...
+                       metricName, columnStartHorzLine)
+        leftMetric_results  = this.metricToString(key, leftMetricValues);
+        rightMetric_results = this.metricToString(key, rightMetricValues);
         fprintf(outputFile, lineFormat, ...
                             linePrefix, metricName, ...
-                            optimizePRBEP_results{1}, ...
-                            optimizePRBEP_results{2}, ...
-                            optimizePRBEP_results{3}, ...
-                            optimizeMacroAcc_results{1},...
-                            optimizeMacroAcc_results{2},...
-                            optimizeMacroAcc_results{3},...
+                            leftMetric_results{1}, ...
+                            leftMetric_results{2}, ...
+                            leftMetric_results{3}, ...
+                            rightMetric_results{1},...
+                            rightMetric_results{2},...
+                            rightMetric_results{3},...
                             columnStartHorzLine ...
                             );
     end
     
     %% metricToString
+    %  Mark highest values with bold. multiply by 100.
+    %  e.g. converts stringValues = { '1'; '2'; '2'; }
+    %  to     '100.0'
+    %         '\textbf{200.0}'
+    %         '\textbf{200.0}'
     
     function R = metricToString(~, key, algorithms)
         stringValues = {    algorithms.mad(key);
                             algorithms.am(key);
                             algorithms.diag(key) };
-        numeriaclValues = 100 * str2num(char(stringValues));
-        [~, maxPosition] = max(numeriaclValues);
-        stringValues = cellstr(num2str(numeriaclValues, '%.1f'));
-        stringValues{maxPosition} = ['\textbf{' stringValues{maxPosition} '}'];
+        numericalValues = 100 * str2num(char(stringValues));
+        % transform 73.21 -> 73.2 (zero out all digits beyond 1 decimal
+        % point)
+        numericalValues = round(numericalValues * 10) / 10; 
+        maxValue = max(numericalValues);
+        positionsWithMaximum = (numericalValues == maxValue);
+        stringValues = cellstr(num2str(numericalValues, '%.1f'));
+        for position_i=1:length(stringValues)
+            if 1 == positionsWithMaximum(position_i)
+                stringValues{position_i} = ['\textbf{' stringValues{position_i} '}'];
+            end
+        end
         R = stringValues;
     end
         
-    %% startTable
+    %% startTable_ecml2012
     
-    function startTable(~, outputFile)
+    function startTable_ecml2012(~, outputFile, optimizeByLeftName, optimizeByRightName)
         fprintf(outputFile, '\\begin{table}\n');
         fprintf(outputFile, '\\centering\n');
         fprintf(outputFile, '\\begin{tabular}{ | c | c || c | c | c || c | c | c | }\n');
         fprintf(outputFile, '\\hline\n');
         fprintf(outputFile, '\\multicolumn{2}{|c||}{}  & \\multicolumn{3}{|c||}{Optimized by} & \\multicolumn{3}{|c|}{Optimized by} \\\\\n');
-        fprintf(outputFile, '\\multicolumn{2}{|c||}{}  & \\multicolumn{3}{|c||}{PRBEP}        & \\multicolumn{3}{|c|}{M-ACC} \\\\ \\cline{3-8}\n');
+        fprintf(outputFile, '\\multicolumn{2}{|c||}{}  & \\multicolumn{3}{|c||}{%s}        & \\multicolumn{3}{|c|}{%s} \\\\ \\cline{3-8}\n', ...
+                            optimizeByLeftName, optimizeByRightName);
         fprintf(outputFile, '\\multicolumn{2}{|c||}{}  & MAD & AM  & \\algorithmName           & MAD & AM  & \\algorithmName \\\\ \n');
     end
     
-    %% endTable
+    %% endTable_ecml2012
     
-    function endTable(this, outputFile, searchProperties )
+    function endTable_ecml2012(this, outputFile, searchProperties )
         
         algorithm.key = 'Algorithm';
         algorithm.shouldMatch = 1;
@@ -1092,10 +1137,14 @@ methods (Access = public)
     
     %% trimValues
     
-    function trimValues(this)
+    function trimValues(this, isTrimLevenshtein)
         trimKeys = { 'avg PRBEP', 'avg accuracy', ...
                      'avg macro accuracy', 'avg MRR', ...
-                     'avg macro MRR', 'avg levenshtein'};
+                     'avg macro MRR'};
+        if isTrimLevenshtein
+           trimKeys{end+1} = 'avg levenshtein';
+        end
+                 
         for result_i=1:length(this.m_resultMaps)
             map = this.m_resultMaps{result_i};
             for key_i = 1:length(trimKeys)
