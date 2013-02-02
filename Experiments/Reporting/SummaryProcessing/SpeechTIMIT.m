e5785a60d337a4edbda7e2cfe33b73aed82f33ab
matanorb <matanorb@4c94cbd0-a001-4ae1-9522-79914504ef8a>
2013-02-02 19:17:56 +0000

    Added code for creating table with results for variants of TACO on TIMIT. ************************************************************************* SpeechGraphs.m: renamed to SpeechTIMIT.m: SpeechTIMIT.m: added gatherResults_tacoVariants(), outputTable(), printSingleLine(), plotDifferencesLocalVsGlobal(), TACO_variants_order(), numLabeledRange(), numLabeledPercentRange()
    git-svn-id: https://matano1-tp.haifa.ibm.com/svn/taco@632 4c94cbd0-a001-4ae1-9522-79914504ef8a

--- Experiments/Reporting/SummaryProcessing/SpeechTIMIT.m
+++ Experiments/Reporting/SummaryProcessing/SpeechTIMIT.m
@@ -1,4 +1,4 @@
-classdef SpeechGraphs < SummaryReaderBase
+classdef SpeechTIMIT < SummaryReaderBase
 
 %% office
 % fileName = 'C:/technion/theses/Tex/SSL/Thesis/Results/thesis_results.txt';
@@ -16,7 +16,7 @@ end
 %% run
 
 function run(fileName)
-    this = SpeechGraphs();
+    this = SpeechTIMIT();
     this.convert(fileName);
 end
 
@@ -37,59 +37,15 @@ methods (Access = private)
 %% create
 
 function create(this)
-    this.createGraphs();
+%     this.createGraphs();
+    this.createTables();
 end
 
-%% plotDifferencesLocalVsGlobal
+%% createTables
 
-function plotDifferencesLocalVsGlobal(this, barSource, fileNameSuffix)
-    algorithmsOrderInSpeechBars = this.algorithmsOrderInSpeechBars();
-
-    fig = figure;
-
-    LOCAL = 1; GLOBAL = 2;
-    bar_position_i = 1;
-    for algorithm_ID = algorithmsOrderInSpeechBars
-        performanceGain(:,bar_position_i) = ...
-            barSource(LOCAL,:,algorithm_ID) - barSource(GLOBAL,:,algorithm_ID); %#ok<AGROW>
-        bar_position_i = bar_position_i +1;
-    end
-
-    h = bar(performanceGain);
-
-    algorithmColors = AlgorithmProperties.algorithmColors();
-    algorithmNames  = AlgorithmProperties.algorithmNames();
-%     dbstop in SpeechGraphs.m at 63;
-    bar_i = 1;
-    for algorithm_ID = algorithmsOrderInSpeechBars
-        singleColor = algorithmColors{algorithm_ID};
-        set(h(bar_i),'facecolor',singleColor); 
-        legendForBars{bar_i} = algorithmNames{algorithm_ID}; %#ok<AGROW>
-        bar_i = bar_i + 1;
-    end
-    
-    set(gca,'XGrid','off','YGrid','on');
-
-    fontSize = 22;
-    set(gca, 'FontSize', fontSize);
-
-    heightAndWidth = [1024 768] * 0.9;
-    figurePosition = [ 1 1 heightAndWidth];
-    set(fig, 'Position', figurePosition); % Maximize figure.
-%         http://dopplershifted.blogspot.co.il/2008/07/programmatically-saving-matlab-figures.html
-%       makes saveas function to not mix up the fonts by resizing the
-%       figure
-    set(fig, 'PaperPositionMode', 'auto');
-
-    set(gca, 'XTickLabel',{'1%', '5%', '10%', '20%' '30%', '50%'});
-    ylabel('Local scaling performance gain');
-    xlabel('Precentage of training set used as labeled data');
-
-    legend(legendForBars, 'Location', 'NorthEast');
-
-    fileNamePrefix = 'timit_performance_gain_' ;
-    this.saveAndCloseFigure(fig,            SpeechGraphs.outputDirectory(), ...
-                            fileNamePrefix, fileNameSuffix);
+function createTables(this)
+    results = this.gatherResults_tacoVariants();
+    this.outputTable(results);
 end
 
 %% createGraphs
@@ -219,7 +175,7 @@ function barSource = gatherData...
         (this, searchProperties, numLabeledRange, ...
          optimizeByKey, presentedKey, contextSize)
     graph.key = 'graph';
-    speechGraphNames = SpeechGraphs.speechGraphNames(contextSize);
+    speechGraphNames = SpeechTIMIT.speechGraphNames(contextSize);
     graph.shouldMatch = 1;
     numGraphs = length(speechGraphNames);
 
@@ -237,11 +193,11 @@ function barSource = gatherData...
 
     for graph_i = 1:numGraphs
         graph.value = speechGraphNames(graph_i);
-        Logger.log(['SpeechGraphs::gatherData. '...
+        Logger.log(['SpeechTIMIT::gatherData. '...
                 'Searching results for graph ''' graph.value{1} '''']);
         for numLabeled_i=1:length(numLabeledRange)
             num_labeled.value = numLabeledRange(numLabeled_i);
-            Logger.log(['SpeechGraphs::gatherData. '...
+            Logger.log(['SpeechTIMIT::gatherData. '...
                 'Searching for results for ' num2str(num_labeled.value{1}) ' labeled.']);
             algorithms = this.findAlgorithms([searchProperties num_labeled graph]);
             barSource(graph_i, numLabeled_i , AlgorithmProperties.MAD) = str2num(algorithms.mad( presentedKey ));
@@ -251,6 +207,115 @@ function barSource = gatherData...
         end
     end
 end        
+
+%% gatherResults_tacoVariants
+
+function results = gatherResults_tacoVariants(this)
+    balanced.key = 'balanced';
+    balanced.value = {'0'};
+    balanced.shouldMatch = 1;
+
+    num_iterations.key = 'max iterations';
+    num_iterations.value = {'20'};
+    num_iterations.shouldMatch = 1;
+
+    labeled_init.key = 'labelled init';        
+    labeled_init.shouldMatch = 1;
+    labeled_init.value = {'1'};
+    
+    heuristics.key = 'heuristics';
+    heuristics.value = {'0'};
+    heuristics.shouldMatch = 1;
+
+    algorithm.key = 'Algorithm';
+    algorithm.shouldMatch = 1;
+    algorithm.value = {CSSLMC.name()};
+    
+    optimize_by.key = 'optimize_by';
+    optimize_by.shouldMatch = 1;
+    optimize_by.value = {'levenshtein'};
+
+    searchProperties = [balanced    labeled_init    num_iterations ...
+                        heuristics  algorithm       optimize_by];
+    
+	num_labeled.key = 'num labeled';
+    num_labeled.shouldMatch = 1;    
+    %     numLabeledPerGraph = this.numLabeledPerGraphForTables();
+
+    taco_objective.key = 'TACO objective';
+    taco_objective.shouldMatch = 1;
+
+    contextSize     = 7;
+    allGraphNames   = this.speechGraphNames(contextSize);
+    numGraphs       = length(allGraphNames);
+    
+    graph.key           = 'graph';
+    graph.shouldMatch   = 1;
+    
+    numLabeledRange      = this.numLabeledRange();
+    TACO_variants_order  = this.TACO_variants_order();
+
+    for graph_i = 1:numGraphs
+        graph.value       = allGraphNames(graph_i);
+        
+        Logger.log(['SpeechTIMIT::gatherResults_tacoVariants. ' ...
+                    'Looking for result on ''' graph.value{1} '''']);
+        
+        numLabeled_i = 1;
+        for numLabeled = numLabeledRange
+            num_labeled.value = {num2str(numLabeled)};
+            for TACO_variant_ID = TACO_variants_order
+                taco_objective.value = {num2str(TACO_variant_ID)};
+                results{graph_i,TACO_variant_ID, numLabeled_i} = ...
+                    this.findEntries([searchProperties num_labeled graph optimize_by taco_objective]); %#ok<AGROW>    
+            end
+            numLabeled_i = numLabeled_i + 1;
+        end
+    end
+end
+
+%% outputTable
+
+function outputTable(this, results)
+    presentedKey         = 'avg levenshtein';
+    
+    numLabeledRange         = this.numLabeledRange();
+    numLabeledPercentRange  = this.numLabeledPercentRange();
+
+    contextSize     = 7;
+    allGraphNames   = this.speechGraphNames(contextSize);
+    
+    for graph_i =1:length(allGraphNames)
+        Logger.log(['SpeechTIMIT::outputTable. graph name = ''' allGraphNames{graph_i} '''']);
+        for numLabeled_i  = 1:length(numLabeledRange)
+            percentLabeled  = numLabeledPercentRange(numLabeled_i);
+            rowKey          = [num2str(percentLabeled) '\%'];
+            lineResults = results(graph_i,:, numLabeled_i);
+            this.printSingleLine(lineResults, presentedKey, rowKey);
+        end        
+    end
+end
+
+%% printSingleLine
+
+function printSingleLine(~, lineResults, presentedKey, rowKey)
+    TACO_variants_order  = SpeechTIMIT.TACO_variants_order();
+    taco_variant_i = 1;
+    for TACO_variant_ID = TACO_variants_order
+        variantResult = lineResults{TACO_variant_ID};
+        variantResult = variantResult{1};
+        stringValues{taco_variant_i}  = variantResult(presentedKey); %#ok<AGROW>
+        taco_variant_i                = taco_variant_i + 1;
+    end
+    stringValues = TextTables.metricToString_internal(stringValues, 1);
+    lineFormat = '&%s & %s & %s & %s & %s \\\\ \\cline{2-6}\n';
+    fprintf( lineFormat, ...., 
+        rowKey, ...
+        stringValues{1}, ...
+        stringValues{2}, ...
+        stringValues{3}, ...
+        stringValues{4} );
+end
 
 %% plotGraph
 
@@ -282,7 +347,7 @@ function plotGraph(this, barSource, numLabeledRange, ...
 
 %         this.removeExtraWhiteSpaceMargin();
 
-    speechGraphNames = SpeechGraphs.speechGraphNames(contextSize);
+    speechGraphNames = SpeechTIMIT.speechGraphNames(contextSize);
     numGraphs = length(speechGraphNames);
 
     fileNamePrefix  = 'timit_';
@@ -291,10 +356,10 @@ function plotGraph(this, barSource, numLabeledRange, ...
     algorithmsOrderInSpeechGraphs = this.algorithmsOrderInSpeechGraphs();
     
     for graph_i = 1:numGraphs
-        fig = this.create_algorithmCompareFigure(yLimits, yLabel, numLabeledRangeAsNumbers);
+        fig = SpeechTIMIT.create_algorithmCompareFigure(yLimits, yLabel, numLabeledRangeAsNumbers);
         allLegendItems = [];
         graphNameForUser = speechGraphNamesForUser{graph_i};
-        Logger.log(['SpeechGraphs::createGraphs. '...
+        Logger.log(['SpeechTIMIT::createGraphs. '...
                     'graph_i = ' num2str(graph_i) '. '...
                     'speechGraphNames(graph_i) = ' speechGraphNames{graph_i}]);
         graphStyle = '-';
@@ -312,14 +377,70 @@ function plotGraph(this, barSource, numLabeledRange, ...
         end
         legend(allLegendItems, 'Location', 'SouthEast');
         fileNameSuffixWithGraphName = [fileNameSuffix '_' graphNameForUser];
-        this.saveAndCloseFigure(fig, SpeechGraphs.outputDirectory(), ...
+        this.saveAndCloseFigure(fig, SpeechTIMIT.outputDirectory(), ...
                                 fileNamePrefix, fileNameSuffixWithGraphName);
     end
 end
 
+%% plotDifferencesLocalVsGlobal
+
+function plotDifferencesLocalVsGlobal(this, barSource, fileNameSuffix)
+    algorithmsOrderInSpeechBars = this.algorithmsOrderInSpeechBars();
+
+    fig = figure;
+
+    LOCAL = 1; GLOBAL = 2;
+    bar_position_i = 1;
+    for algorithm_ID = algorithmsOrderInSpeechBars
+        performanceGain(:,bar_position_i) = ...
+            barSource(LOCAL,:,algorithm_ID) - barSource(GLOBAL,:,algorithm_ID); %#ok<AGROW>
+        bar_position_i = bar_position_i +1;
+    end
+
+    h = bar(performanceGain);
+
+    algorithmColors = AlgorithmProperties.algorithmColors();
+    algorithmNames  = AlgorithmProperties.algorithmNames();
+%     dbstop in SpeechTIMIT.m at 63;
+    bar_i = 1;
+    for algorithm_ID = algorithmsOrderInSpeechBars
+        singleColor = algorithmColors{algorithm_ID};
+        set(h(bar_i),'facecolor',singleColor); 
+        legendForBars{bar_i} = algorithmNames{algorithm_ID}; %#ok<AGROW>
+        bar_i = bar_i + 1;
+    end
+    
+    set(gca,'XGrid','off','YGrid','on');
+
+    fontSize = 22;
+    set(gca, 'FontSize', fontSize);
+
+    heightAndWidth = [1024 768] * 0.9;
+    figurePosition = [ 1 1 heightAndWidth];
+    set(fig, 'Position', figurePosition); % Maximize figure.
+%         http://dopplershifted.blogspot.co.il/2008/07/programmatically-saving-matlab-figures.html
+%       makes saveas function to not mix up the fonts by resizing the
+%       figure
+    set(fig, 'PaperPositionMode', 'auto');
+
+    set(gca, 'XTickLabel',{'1%', '5%', '10%', '20%' '30%', '50%'});
+    ylabel('Local scaling performance gain');
+    xlabel('Precentage of training set used as labeled data');
+
+    legend(legendForBars, 'Location', 'NorthEast');
+
+    fileNamePrefix = 'timit_performance_gain_' ;
+    this.saveAndCloseFigure(fig,            SpeechTIMIT.outputDirectory(), ...
+                            fileNamePrefix, fileNameSuffix);
+end
+
+end % private methods 
+
+methods (Static)
+
 %% create_algorithmCompareFigure
 
-function fig = create_algorithmCompareFigure(~, yLimits, yLabel, numLabeledRangeAsNumbers)
+function fig = create_algorithmCompareFigure(yLimits, yLabel, numLabeledRangeAsNumbers)
     fig = figure;
     hold on;
 
@@ -346,11 +467,7 @@ function fig = create_algorithmCompareFigure(~, yLimits, yLabel, numLabeledRange
 %       makes saveas function to not mix up the fonts by resizing the
 %       figure
     set(fig, 'PaperPositionMode', 'auto');
-end
-
-end % private methods 
-
-methods (Static)
+end    
     
 %% speechGraphNames
 
@@ -378,6 +495,28 @@ end
 function R = algorithmsOrderInSpeechBars()
     R = [AlgorithmProperties.CSSL AlgorithmProperties.AM ...
          AlgorithmProperties.MAD  AlgorithmProperties.QC];
+end
+
+%% TACO_variants_order
+
+function R = TACO_variants_order()
+    R = [CSSLBase.OBJECTIVE_HARMONIC_MEAN_SINGLE ...
+         CSSLBase.OBJECTIVE_WEIGHTS_UNCERTAINTY_SINGLE ...
+         CSSLBase.OBJECTIVE_HARMONIC_MEAN        ...CSSLBase.OBJECTIVE_MULTIPLICATIVE             ...
+         CSSLBase.OBJECTIVE_WEIGHTS_UNCERTAINTY  ...
+          ];
+end
+
+%% numLabeledRange
+
+function R = numLabeledRange()
+    R = [11147 55456  111133 221254 331793  553041];
+end
+
+%% numLabeledPercentRange
+
+function R = numLabeledPercentRange()
+    R = [1 5  10 20 30  50];
 end
 
 end % static methods
